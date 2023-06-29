73d4a5d1b9b40c68d6be2f9789f5f3182194cd24
Zchack <gustavo.a.c.m@gmail.com>
2023-06-29 18:13:13 -0500

    chunks updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Establece un valor más alto para el límite de advertencia del tamaño del fragmento
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Define aquí los fragmentos manuales según tus necesidades
+        }
+      }
+    }
+  }
 })
+
+
+
 
