544e10d1f672d50804ca37cda9b3deab978d3561
Zchack <gustavo.a.c.m@gmail.com>
2023-07-04 20:52:29 -0500

    corrections typewriter componenttt

--- src/components/TypeWriter.jsx
+++ src/components/TypeWriter.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import "../css/globals.scss";
-const titulo = document.getElementById('titulo');
+
 export const TypeWriter = ({ text }) => {
   const [charactersWritten, setCharactersWritten] = useState(0);
+   document.getElementById('titulo');
 
   useEffect(() => {
     const timeout = setTimeout(() => {
